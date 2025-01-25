24102a1fb7dfab92fde1330489c766b791d6ed5e
Samuel Baquero <samuelbaquero@192.168.1.2>
2025-01-25 05:48:46 -0500

    try

--- server.js
+++ server.js
@@ -13,6 +13,7 @@ app.use(cors({
     methods: ['GET', 'POST', 'PUT', 'DELETE'],
     allowedHeaders: ['Content-Type', 'Authorization'],
 }));
+app.options('*', cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
